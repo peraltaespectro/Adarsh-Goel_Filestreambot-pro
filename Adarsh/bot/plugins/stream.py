b723e14496de8d9c8b1a352370d3726bf0db386f
Peralta Espectro <109923346+peraltaespectro@users.noreply.github.com>
2022-09-16 21:06:20 -0300

    Update stream.py

--- Adarsh/bot/plugins/stream.py
+++ Adarsh/bot/plugins/stream.py
@@ -124,7 +124,7 @@ async def private_receive_handler(c: Client, m: Message):
 
         await log_msg.reply_text(text=f"**REQUERIDO POR :** [{m.from_user.first_name}](tg://user?id={m.from_user.id})\n**ID DO USUÁRIO :** `{m.from_user.id}`\n**Link de Streaming :** {stream_link}", disable_web_page_preview=True,  quote=True)
         await m.reply_text(
-            text=msg_text.format(get_name(log_msg), humanbytes(get_media_file_size(m)), online_link, stream_link),
+            text=msg_text.format(humanbytes(get_media_file_size(m)), online_link, stream_link),
             quote=True,
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🖥  ASSISTIR  🖥", url=stream_link), #Stream Link
